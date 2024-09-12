e24b87496bd8d648fed56ee62dd3ac560609c781
Kevin2606-code <kevinandika897@gmail.com>
2024-09-12 11:06:21 +0700

    register baru

--- src/modules/register/RegisterView.js
+++ src/modules/register/RegisterView.js
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import React from 'react';
 
 export default function Register() {
   return (
-    <View>
-      <Text>Register</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+      }}
+    >
+      <Text>Register Jarbi</Text>
+      <Image source={require('../../../assets/images/logo.png')} />
     </View>
   );
 }
