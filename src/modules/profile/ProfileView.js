2cc49d68a4b9a61176c532eccdad695123a436b0
AchmadArifaturrohman <140537284+AchmadArifaturrohman@users.noreply.github.com>
2024-09-12 11:46:22 +0700

    Profile

--- src/modules/profile/ProfileView.js
+++ src/modules/profile/ProfileView.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function ProfileView() {
   return (
     <View>
-      <Text>ProfileView</Text>
+      <Text>PancegLampahDinaGalur</Text>
     </View>
   )
 }
